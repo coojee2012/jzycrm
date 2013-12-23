2f82b84a37dc4ca9b33ec767ad51db3eb73c4198
coojee2012 <11366846@qq.com>
2013-12-23 17:46:47 +0800

    修正BUG

--- sccincc/app.js
+++ sccincc/app.js
@@ -51,7 +51,7 @@ var mysql = require('mysql').createConnection({
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -208,7 +208,7 @@ app.get('*', function(req, res){
  */
 
 if (!module.parent) {
-	app.listen(3000);
+	app.listen(80);
 	console.log("Express server listening on port %d in %s mode", app
 		.get('port'), app.settings.env);
 }
